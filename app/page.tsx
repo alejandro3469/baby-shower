9a9a7e7213a8166efd1134e4feb106928900e1fe
alejandro3469 <alejandro.perez3469@gmail.com>
2024-06-17 20:43:44 -0600

    text

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { motion } from "framer-motion";
+import { Poppins, Dancing_Script } from 'next/font/google'
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+})
+const dancing_Script = Dancing_Script({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={`${poppins.className} ${styles.main}`}>
+      <div>
+        <h1>
+          <span className="baby">Baby </span><span className={`${dancing_Script.className} shower`}>shower</span>
+        </h1>
+      </div>
       {<motion.img
         animate={{ y: [0, 60, 0] }}
         transition={{ repeat: Infinity, duration: 3 }}
