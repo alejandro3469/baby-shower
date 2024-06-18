c69f5395a4389ac77c25e37067b40921a17eaebd
alejandro3469 <alejandro.perez3469@gmail.com>
2024-06-17 20:15:56 -0600

    winnie moves

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <img src="/winnie.png" />
+      <motion.img
+        animate={{ y: [0, 60, 0] }}
+        transition={{ repeat: Infinity, duration: 3 }}
+        className="banner-main-image"
+        src="/winnie.png"
+      />
     </main>
   );
 }
